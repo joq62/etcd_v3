9215c316b6afa8a1f1d5a4a444c4fbcdb2255c1b
joq62 <joakim.leche@gmail.com>
2022-08-01 23:57:31 +0200

    logs_db_dynamic

--- src/dynamic_db.erl
+++ src/dynamic_db.erl
@@ -52,8 +52,12 @@ start([])->
     R2={db_deployment_info:create_table(),db_deployment_info},
     R3={db_deployments:create_table(),db_deployments},
     R4={db_host_spec:create_table(),db_host_spec},
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DBG: CreateTable   ",[R1,R2,R3,R4],?MODULE,node()}]), 
     [R1,R2,R3,R4];
 start(EtcdNodes) ->
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DBG: EtcdNodes   ",EtcdNodes,?MODULE,node()}]), 
     add_extra_nodes(EtcdNodes).
 
 
@@ -65,15 +69,28 @@ start(EtcdNodes) ->
 add_extra_nodes([Node|T])->
     case mnesia:change_config(extra_db_nodes,[Node]) of
 	{ok,[Node]}->
-	    mnesia:add_table_copy(schema,node(),?StorageType),
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: add_extra_nodes Node at node()  ",Node,?MODULE,node()}]), 
+ 
+	    AddSchema=mnesia:add_table_copy(schema,node(),?StorageType),
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: add_extra_nodes AddSchema  ",AddSchema,?MODULE,node()}]), 
 %	    TablesFromNode=rpc:call(Node,mnesia,system_info,[tables]),
 %	    [mnesia:add_table_copy(Table,node(),?StorageType)||Table<-TablesFromNode,
 %							       Table/=schema],
-	    [mnesia:add_table_copy(Table,node(),?StorageType)||Table<-?TablesToCopy,
+	    AddTableCopies=[{Table,mnesia:add_table_copy(Table,node(),?StorageType)}||Table<-?TablesToCopy,
 								 Table/=schema],
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: add_extra_nodes AddTableCopies  ",AddTableCopies,?MODULE,node()}]), 
 	    Tables=mnesia:system_info(tables),
-	    mnesia:wait_for_tables(Tables,?WAIT_FOR_TABLES);
-	_->
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: add_extra_nodes Tables  ",Tables,?MODULE,node()}]),
+	    WaitForTables=mnesia:wait_for_tables(Tables,?WAIT_FOR_TABLES),
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: add_extra_nodes WaitForTables  ",WaitForTables,?MODULE,node()}]);
+	Reason->
+	    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+					 {"DBG: Didnt connect to Node Reason  ",Reason,?MODULE,node()}]),
 	    add_extra_nodes(T)
     end.
     
